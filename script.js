82e1766e2b7047f8cf186d0588c4ebad497d4810
Chefia777 <ariel_robinson_moreira@hotmail.com>
2022-10-05 12:00:28 -0300

    atualizacoes

--- script.js
+++ script.js
@@ -1,63 +1,100 @@
-var formulario = document.querySelector('form')
-let imagemInicial = document.getElementById("imginicial")
-let urlForm = "https://pokeapi.co/api/v2/pokemon/";
-let botao = document.getElementById("botaozinho");
+function pegarPokemon() {
+    var formulario = document.querySelector('form');
 
+    let imagemInicial = document.getElementById("imginicial");
 
-let idPokemon = Math.floor(Math.random()*151);
+    let urlForm = "https://pokeapi.co/api/v2/pokemon/";
 
-// Valor do inpt Name
-let nome = document.getElementById("name");
+    let botao = document.getElementById("botaozinho");
 
-// Concatena a url com o inputname
-urlForm = urlForm + idPokemon;
+    let botaoNovo = document.getElementById("botaozinho-novo")
 
-// Transforma os valores em minúsculas
-urlForm = urlForm.toLocaleLowerCase()
+    let idPokemon = Math.floor(Math.random()*151);
 
-// ID Content
-let resposta = document.getElementById('content')
+    // Valor do inpt Name
+    let nome = document.getElementById("name");
 
-// Id ImgPokemon
-let imagem = document.getElementById('imgPokemon')
+    // Concatena a url com o inputname
+    urlForm = urlForm + idPokemon;
 
-let conteudo = document.getElementById('content')
+    // Transforma os valores em minúsculas
+    urlForm = urlForm.toLocaleLowerCase();
 
-// Resposta em HTML
-let html = ''
+    // ID Content
+    let resposta = document.getElementById('content');
 
-let nomePokemon = ''
+    // Id ImgPokemon
+    let imagem = document.getElementById('imgPokemon');
 
-fetch(urlForm)
+    let conteudo = document.getElementById('content');
+
+    // Resposta em HTML
+    let html = '';
+
+    let nomePokemon = '';
+
+    fetch(urlForm)
         .then(resposta => resposta.json())
         .then(function (data) {
-            html = 'Nome: ' + data.name
-            resposta.innerHTML = html
+            html = 'Nome: ' + data.name;
+            resposta.innerHTML = html;
             imagemInicial.innerHTML = "<img src='" + data.sprites.front_default + "'>"
         })
         .catch(function (err) {
             if(err == 'SyntaxError: Unexpected token N in JSON at position 0'){
                 html = 'Pokémon não encontrado! 😒'
             } else {
-                html = err
+                html = err;
             }
-            resposta.innerHTML = html
+            resposta.innerHTML = html;
         })
 
-formulario.addEventListener('submit', function(e) {
-    e.preventDefault(),false
-})
+        formulario.addEventListener('submit', function(e) {
+            e.preventDefault()
+            ,false
+        })
+        
+        botao.addEventListener('click', function (event) {   
+            event.preventDefault();
+            if("Nome: " + document.getElementById("name").value == html) {
+                console.log("passou")
+                imagemInicial.classList.remove("imagem");
+                imagemInicial.classList.add("imagem-revelada");
+                botao.classList.add("esconder");
+                botao.parentElement.classList.add("esconder");
+                botaoNovo.classList.remove("esconder");
+                botaoNovo.parentElement.classList.remove("esconder");
+                botaoNovo.parentElement.classList.add("group-form");
+                conteudo.classList.remove("conteudo");
+                conteudo.classList.add("conteudo-ativado")
+                return false;
+            }
+            false;
+        });
 
-botao.addEventListener('click', function (event) {   
-    event.preventDefault();
-    if("Nome: " + document.getElementById("name").value == html) {
-        console.log("passou")
-        imagemInicial.classList.remove("imagem");
-        imagemInicial.classList.add("imagem-revelada");
-        conteudo.classList.remove("conteudo");
-        conteudo.classList.add("conteudo-ativado")
-        return false;
+        botaoNovo.addEventListener('click', function (event) {  
+            pegarPokemon(); 
+            event.preventDefault();
+            nome.value = '';
+            imagemInicial.classList.remove("imagem-revelada");
+            imagemInicial.classList.add("imagem");
+            botao.classList.remove("esconder");
+            botao.classList.add("group-form");
+            botao.parentElement.classList.remove("esconder");
+            botao.parentElement.classList.add("group-form");
+            botaoNovo.classList.remove("group-form");
+            botaoNovo.classList.add("esconder");
+            botaoNovo.parentElement.classList.remove("group-form");
+            botaoNovo.parentElement.classList.add("esconder");
+            conteudo.classList.remove("conteudo-ativado");
+            conteudo.classList.add("conteudo");
+            return false;
+        });
+
     }
-    false;
-    
-});
+
+
+
+
+
+pegarPokemon();
